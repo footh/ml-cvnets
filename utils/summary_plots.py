dd3f6eee47a867b4db78d87780d1ddf6d8cd6fb4
zanker99 <auppuluri9@gmail.com>
2022-04-29 09:25:06 -0700

    depth 18 augmentation 4 divbyten lr0pt4 3 step run

--- utils/summary_plots.py
+++ utils/summary_plots.py
@@ -206,7 +206,8 @@ def main(argv):
     #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth50_lr0pt4__divbytenlr.txt"
     #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth50_augment4_divbytenlr.txt"
     #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth50_augment4_divbyten_lr0pt1_3step.txt"
-    input_run_file = "results\AU_results_resnet_tiny_correct_class_depth50_augment4_divbyten_lr0pt4_3step.txt"
+    #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth50_augment4_divbyten_lr0pt4_3step.txt"
+    input_run_file = "results\AU_results_resnet_tiny_correct_class_depth18_augment4_divbyten_lr0pt4_3step.txt"
     
     input_mobileViT_csv = None
     if len(argv) < 1: 
