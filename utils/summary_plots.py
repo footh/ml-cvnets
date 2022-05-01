2b11d27f2d1b973ab5ba2e95a8267a3f2f11bac0
zanker99 <auppuluri9@gmail.com>
2022-04-30 22:38:29 -0700

    depth 18 aug 5 tiny cosine vbs1

--- utils/summary_plots.py
+++ utils/summary_plots.py
@@ -209,10 +209,11 @@ def main(argv):
     #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth50_augment4_divbyten_lr0pt4_3step.txt"
     #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth18_augment4_divbyten_lr0pt4_3step.txt"
     #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth34.txt"
-    #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth18_vbs1.txt"
+    #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth34_vbs1.txt"
     #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth18_augment4_divbyten_lr0pt4_3step_vbs1.txt"
     #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth50_vbs1.txt"
-    input_run_file = "results\AU_results_resnet_tiny_correct_class_depth18_vbs1.txt"
+    #input_run_file = "results\AU_results_resnet_tiny_correct_class_depth18_vbs1.txt"
+    input_run_file = "results\AU_results_resnet_tiny_correct_class_depth18_augment5_tiny_cosine_vbs1.txt"
     
     input_mobileViT_csv = None
     if len(argv) < 1: 
