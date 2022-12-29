c1e951f1e6d63d5868e544333a355067425e8ce5
zanker99 <auppuluri9@gmail.com>
2022-12-29 02:27:17 -0800

    unused but useful scripts

---
+++ data/datasets/classification/tinyimagenet.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# [1] http://cs231n.stanford.edu/reports/2017/pdfs/930.pdf
+# [2] https://github.com/rmccorm4/Tiny-Imagenet-200 
+# [3] https://gist.github.com/moskomule/2e6a9a463f50447beca4e64ab4699ac4 
+
+# download and unzip dataset
+#wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
+unzip tiny-imagenet-200.zip
+
+current="$(pwd)/tiny-imagenet-200"
+
+# training data
+cd $current/train
+for DIR in $(ls); do
+   cd $DIR
+   rm *.txt
+   mv images/* .
+   rm -r images
+   cd ..
+done
+
+# validation data
+cd $current/val
+annotate_file="val_annotations.txt"
+length=$(cat $annotate_file | wc -l)
+for i in $(seq 1 $length); do
+    # fetch i th line
+    line=$(sed -n ${i}p $annotate_file)
+    # get file name and directory name
+    file=$(echo $line | cut -f1 -d" " )
+    directory=$(echo $line | cut -f2 -d" ")
+    mkdir -p $directory
+    mv images/$file $directory
+done
+rm -r images
+echo "done"
